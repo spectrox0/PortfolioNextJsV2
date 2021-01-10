cb8b6957c1cc1436f4deb782ad905547267a4be9
spectrox0 <alejanvelazco2008@hotmail.com>
2021-01-10 11:45:31 -0400

    ..

--- components/Skills/index.tsx
+++ components/Skills/index.tsx
@@ -1,231 +1,290 @@
 import React from 'react'
 import Skill from './Skill'
 import {
-  FaVuejs,
-  FaReact,
-  FaAngular,
-  FaPython,
-  FaJava,
-  FaJs,
-  FaNode,
-  FaSymfony,
-  FaLaravel,
-  FaBootstrap,
-  FaPhp,
+    FaAngular,
+    FaBootstrap,
+    FaJava,
+    FaJs,
+    FaLaravel,
+    FaNode,
+    FaPhp,
+    FaPython,
+    FaReact,
+    FaSymfony,
+    FaVuejs,
 } from 'react-icons/fa'
+import {DiCss3, DiDjango, DiFirebase, DiMongodb, DiMysql,} from 'react-icons/di'
+import {GrGraphQl} from 'react-icons/gr'
 import {
-  DiMysql,
-  DiDjango,
-  //DiRuby,
-  DiMongodb,
-  DiCss3,
-  DiFirebase,
-} from 'react-icons/di'
-import { GrGraphQl } from 'react-icons/gr'
-import { GiArtificialIntelligence } from 'react-icons/gi'
+    SiAnaconda,
+    SiFlask,
+    SiKeras,
+    SiNextDotJs,
+    SiNuxtDotJs,
+    SiPytorch,
+    SiSocketDotIo,
+    SiTensorflow,
+    SiTypescript
+} from 'react-icons/si'
+import {GiArtificialIntelligence} from 'react-icons/gi'
 import styled from 'styled-components'
-import { device, primaryColor } from '../../helpers/styles'
+import {device, primaryColor} from '../../helpers/styles'
 import PerfectScroll from 'react-perfect-scrollbar'
-import { motion } from 'framer-motion'
+import {motion} from 'framer-motion'
 import {Title} from '../../styles/titles'
 import {Btn} from '../../styles/buttons'
+
 interface skill {
-  name: string,
-  icon: React.ReactElement,
-  progress: number,
-  categories: string[]
+    name: string,
+    icon: React.ReactElement,
+    progress: number,
+    categories: string[]
 }
 
 const skills: skill[] = [
-  {
-    name: 'Vue',
-    icon: <FaVuejs />,
-    progress: 70,
-    categories: ['FRONTEND', 'FRAMEWORKS/LIBRARIES'],
-  },
-  {
-    name: 'React',
-    icon: <FaReact />,
-    progress: 80,
-    categories: ['FRONTEND', 'FRAMEWORKS/LIBRARIES'],
-  },
-  {
-    name: 'Angular',
-    icon: <FaAngular />,
-    progress: 60,
-    categories: ['FRONTEND', 'FRAMEWORKS/LIBRARIES'],
-  },
-  {
-    name: 'Python',
-    icon: <FaPython />,
-    progress: 60,
-    categories: ['LANGUAGES', 'BACKEND'],
-  },
+    {
+        name: 'Vue',
+        icon: <FaVuejs/>,
+        progress: 70,
+        categories: ['FRONTEND', 'FRAMEWORKS/LIBRARIES'],
+    },
+    {
+        name: 'React',
+        icon: <FaReact/>,
+        progress: 80,
+        categories: ['FRONTEND', 'FRAMEWORKS/LIBRARIES'],
+    }, {
+        name: 'Socket.IO',
+        icon: <SiSocketDotIo/>,
+        progress: 60,
+        categories: ['BACKEND', 'FRAMEWORKS/LIBRARIES'],
+    },
+    {
+        name: 'Nuxt',
+        icon: <SiNuxtDotJs/>,
+        progress: 70,
+        categories: ['FRONTEND', 'FRAMEWORKS/LIBRARIES']
+    },
+    {
+        name: 'Next',
+        icon: <SiNextDotJs/>,
+        progress: 80,
+        categories: ['FRONTEND', 'FRAMEWORKS/LIBRARIES']
+    },
+    {
+        name: 'Angular',
+        icon: <FaAngular/>,
+        progress: 60,
+        categories: ['FRONTEND', 'FRAMEWORKS/LIBRARIES'],
+    },
+    {
+        name: 'Python',
+        icon: <FaPython/>,
+        progress: 60,
+        categories: ['LANGUAGES', 'BACKEND', 'DATA SCIENCE'],
+    },
+    {
+        name: 'Pandas',
+        icon: <GiArtificialIntelligence/>,
+        progress: 50,
+        categories: ['DATA SCIENCE'],
+    },
+    {
+        name: 'Pytorch',
+        icon: <SiPytorch/>,
+        progress: 30,
+        categories: ['DATA SCIENCE'],
+    },
+    {
+        name: 'Anaconda',
+        icon: <SiAnaconda/>,
+        progress: 40,
+        categories: ['DATA SCIENCE'],
+    },
+    {
+        name: 'Keras',
+        icon: <SiKeras/>,
+        progress: 30,
+        categories: ['DATA SCIENCE'],
+    },
+    {
+        name: 'Tensorflow',
+        icon: <SiTensorflow/>,
+        progress: 30,
+        categories: ['DATA SCIENCE'],
+    },
 
-  {
-    name: 'Pytorch',
-    icon: <GiArtificialIntelligence />,
-    progress: 30,
-    categories: ['MACHINE LEARNING'],
-  },
+    {
+        name: 'Java',
+        icon: <FaJava/>,
+        progress: 50,
+        categories: ['BACKEND', 'LANGUAGES'],
+    },
+    {
+        name: 'Php',
+        icon: <FaPhp/>,
+        progress: 40,
+        categories: ['BACKEND', 'LANGUAGES'],
+    },
 
-  {
-    name: 'Java',
-    icon: <FaJava />,
-    progress: 50,
-    categories: ['BACKEND', 'LANGUAGES'],
-  },
-  {
-    name: 'Php',
-    icon: <FaPhp />,
-    progress: 40,
-    categories: ['BACKEND', 'LANGUAGES'],
-  },
-
-  {
-    name: 'Bootstrap',
-    icon: <FaBootstrap />,
-    progress: 60,
-    categories: ['FRONTEND', 'FRAMEWORKS/LIBRARIES'],
-  },
-  {
-    name: 'Django',
-    icon: <DiDjango />,
-    progress: 60,
-    categories: ['BACKEND', 'FRAMEWORKS/LIBRARIES'],
-  },
-  {
-    name: 'Symfony',
-    icon: <FaSymfony />,
-    progress: 40,
-    categories: ['BACKEND', 'FRAMEWORKS/LIBRARIES'],
-  },
-  {
-    name: 'Laravel',
-    icon: <FaLaravel />,
-    progress: 30,
-    categories: ['BACKEND', 'FRAMEWORKS/LIBRARIES'],
-  },
-  {
-    name: 'SQL',
-    icon: <DiMysql />,
-    progress: 50,
-    categories: ['BACKEND', 'LANGUAGES'],
-  },
-  {
-    name: 'MongoDB',
-    icon: <DiMongodb />,
-    progress: 50,
-    categories: ['BACKEND', 'LANGUAGES'],
-  },
-  {
-    name: 'Firebase',
-    icon: <DiFirebase />,
-    progress: 50,
-    categories: ['FRONTEND', 'FRAMEWORKS/LIBRARIES'],
-  },
-  {
-    name: 'React Native',
-    icon: <FaReact />,
-    progress: 50,
-    categories: ['FRONTEND', 'FRAMEWORKS/LIBRARIES'],
-  },
-  {
-    name: 'Node.js',
-    icon: <FaNode />,
-    progress: 50,
-    categories: ['BACKEND'],
-  },
-  {
-    name: 'Javascript',
-    icon: <FaJs />,
-    progress: 75,
-    categories: ['FRONTEND', 'LANGUAGES'],
-  },
-
-  {
-    name: 'CSS3',
-    icon: <DiCss3 />,
-    progress: 80,
-    categories: ['FRONTEND', 'LANGUAGES', 'UI/UX DESIGN'],
-  },
+    {
+        name: 'Bootstrap',
+        icon: <FaBootstrap/>,
+        progress: 60,
+        categories: ['FRONTEND', 'FRAMEWORKS/LIBRARIES'],
+    },
+    {
+        name: 'Django',
+        icon: <DiDjango/>,
+        progress: 60,
+        categories: ['BACKEND', 'FRAMEWORKS/LIBRARIES'],
+    },
+    {
+        name: 'Flask',
+        icon: <SiFlask/>,
+        progress: 60,
+        categories: ['BACKEND', 'FRAMEWORKS/LIBRARIES'],
+    },
+    {
+        name: 'Symfony',
+        icon: <FaSymfony/>,
+        progress: 40,
+        categories: ['BACKEND', 'FRAMEWORKS/LIBRARIES'],
+    },
+    {
+        name: 'Laravel',
+        icon: <FaLaravel/>,
+        progress: 30,
+        categories: ['BACKEND', 'FRAMEWORKS/LIBRARIES'],
+    },
+    {
+        name: 'SQL',
+        icon: <DiMysql/>,
+        progress: 50,
+        categories: ['BACKEND', 'LANGUAGES'],
+    },
+    {
+        name: 'MongoDB',
+        icon: <DiMongodb/>,
+        progress: 50,
+        categories: ['BACKEND', 'LANGUAGES'],
+    },
+    {
+        name: 'Firebase',
+        icon: <DiFirebase/>,
+        progress: 50,
+        categories: ['FRONTEND', 'FRAMEWORKS/LIBRARIES'],
+    },
+    {
+        name: 'React Native',
+        icon: <FaReact/>,
+        progress: 50,
+        categories: ['FRONTEND', 'FRAMEWORKS/LIBRARIES'],
+    },
+    {
+        name: 'Node.js',
+        icon: <FaNode/>,
+        progress: 50,
+        categories: ['BACKEND'],
+    },
+    {
+        name: 'Javascript',
+        icon: <FaJs/>,
+        progress: 75,
+        categories: ['FRONTEND', 'BACKEND', 'LANGUAGES'],
+    },
+    {
+        name: 'Typescript',
+        icon: <SiTypescript/>,
+        progress: 70,
+        categories: ['FRONTEND', 'BACKEND', 'LANGUAGES'],
+    },
 
 
-  {
-    name: 'GraphQL',
-    icon: <GrGraphQl />,
-    progress: 70,
-    categories: ['BACKEND'],
-  },
+    {
+        name: 'CSS3',
+        icon: <DiCss3/>,
+        progress: 80,
+        categories: ['FRONTEND', 'LANGUAGES', 'UI/UX DESIGN'],
+    },
+
+
+    {
+        name: 'GraphQL',
+        icon: <GrGraphQl/>,
+        progress: 70,
+        categories: ['BACKEND'],
+    },
 ]
 const ops: string[] = [
-  'FRONTEND',
-  'BACKEND',
-  'FRAMEWORKS/LIBRARIES',
-  'LANGUAGES',
+    'FRONTEND',
+    'BACKEND',
+    'FRAMEWORKS/LIBRARIES',
+    'DATA SCIENCE',
+    'LANGUAGES',
 ]
-export default function Skills() {
+const Skills: React.FC = () => {
 
 
-  const [currentOp, setCurrentOp] = React.useState<number>(0)
+    const [currentOp, setCurrentOp] = React.useState<number>(0)
 
-  const SkillVariant = {
-    initial: {
-      opacity: 0,
-    },
-    enter: {
-      opacity: 1,
-      transition: {
-        duration: 0.4,
-        ease: [0.48, 0.15, 0.25, 0.96],
-      },
-    },
-    exit: {
-      opacity: 0,
-    },
+    const SkillVariant = {
+        initial: {
+            opacity: 0,
+        },
+        enter: {
+            opacity: 1,
+            transition: {
+                duration: 0.4,
+                ease: [0.48, 0.15, 0.25, 0.96],
+            },
+        },
+        exit: {
+            opacity: 0,
+        },
 
-  }
-  return (
-    <SkillS
-      id="skills"
-      animate="enter"
-      initial="initial"
-      exit="exit"
-      variants={SkillVariant}
-      transition={{ duration: 0.5 }}
-    >
-      <PerfectScroll>
-        <div className="container">
-          <Title>
-            My <span className='word2'> Skills</span>
-          </Title>
-          <div className="wrappers">
-            <div className="switch">
-              {ops.map((item, i) => (
-                <Btn
-                  key={item}
-                  flexGrow={1}
-                  className={i == currentOp ? 'active' : ''}
-                  onClick={() => setCurrentOp(i)}
-                >
-                  {item}
-                </Btn>
-              ))}
-            </div>
-            <div className="col-skills">
-              <div className="skills-container">
-                {skills
-                  .filter(skill => skill.categories.includes(ops[currentOp]))
-                  .map((props, index) => (
-                    <Skill delay={0.5 * index} {...props} key={props.name} />
-                  ))}
-              </div>
-            </div>
-          </div>
-        </div>
-      </PerfectScroll>
-    </SkillS>
-  )
+    }
+    return (
+        <SkillS
+            id="skills"
+            animate="enter"
+            initial="initial"
+            exit="exit"
+            variants={SkillVariant}
+            transition={{duration: 0.5}}
+        >
+            <PerfectScroll>
+                <div className="container">
+                    <Title>
+                        My <span className='word2'> Skills</span>
+                    </Title>
+                    <div className="wrappers">
+                        <div className="switch">
+                            {ops.map((item, i) => (
+                                <Btn
+                                    key={item}
+                                    flexGrow={1}
+                                    className={i == currentOp ? 'active' : ''}
+                                    onClick={() => setCurrentOp(i)}
+                                >
+                                    {item}
+                                </Btn>
+                            ))}
+                        </div>
+                        <div className="col-skills">
+                            <div className="skills-container">
+                                {skills
+                                    .filter(skill => skill.categories.includes(ops[currentOp]))
+                                    .map((props, index) => (
+                                        <Skill delay={0.5 * index} {...props} key={props.name}/>
+                                    ))}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </PerfectScroll>
+        </SkillS>
+    )
 }
 
 const SkillS = styled(motion.section)`
@@ -239,6 +298,7 @@ const SkillS = styled(motion.section)`
   z-index: 6;
   width: 100%;
   overflow-y: auto;
+
   &::-webkit-scrollbar {
     width: 0.7em;
     height: 1em;
@@ -248,6 +308,7 @@ const SkillS = styled(motion.section)`
   &::-webkit-scrollbar-thumb {
     background-color: transparent;
     border: solid 1px rgb(240, 240, 240);
+
     &:hover {
       border-color: ${primaryColor};
     }
@@ -258,11 +319,13 @@ const SkillS = styled(motion.section)`
     position: fixed;
     border-left: solid 1px rgb(240, 240, 240);
   }
+
   .container {
     flex: 1;
     padding: 2rem 1.5rem;
     position: relative;
     z-index: 4;
+
     h2 {
       letter-spacing: 0.15em;
       font-size: 2.5em;
@@ -272,6 +335,7 @@ const SkillS = styled(motion.section)`
 
       font-weight: 300;
       margin-bottom: 1rem;
+
       span {
         font-weight: 500;
         color: ${primaryColor};
@@ -281,34 +345,39 @@ const SkillS = styled(motion.section)`
     .wrappers {
       display: flex;
       flex: 1;
+
       button {
         width: 100%;
       }
+
       @media (max-width: 992px) {
         flex-direction: column;
         .switch {
           display: flex;
           flex-wrap: wrap;
-          button{
+
+          button {
             width: auto;
           }
+
           width: auto;
         }
       }
     }
+
     .switch {
       flex-basis: 35%;
       margin: 1rem 0;
       margin-bottom: 2rem;
       position: relative;
-      
+
     }
+
     .col-skills {
       flex: 1;
     }
-    .skills-container {
-      grid-row-gap: 0;
 
+    .skills-container {
       display: grid;
       grid-template-columns: repeat(2, 1fr);
       grid-row-gap: 2rem;
@@ -321,3 +390,5 @@ const SkillS = styled(motion.section)`
     }
   }
 `
+
+export default Skills;
