f74e2e0057ad65525eb8baf83917c2ed8e5f3c17
alejandro <alejanvelazco2008@hotmail.com>
2021-04-26 09:10:22 -0400

    .

--- components/organism/Skills/index.tsx
+++ components/organism/Skills/index.tsx
@@ -17,15 +17,18 @@ const Skills: React.FC = (): JSX.Element => {
     const SkillVariant = {
         initial: {
             opacity: 0,
+            y:50,
         },
         enter: {
             opacity: 1,
+            y:0,
             transition: {
                 duration: 0.4,
                 ease: [0.48, 0.15, 0.25, 0.96],
             },
         },
         exit: {
+            y:0,
             opacity: 0,
         },
 
